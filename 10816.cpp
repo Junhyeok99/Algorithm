767537665ee95518856d4951ccd6f9604a543661
Junhyeok99 <apple2870907@gmail.com>
2020-07-13 03:52:24 +0900

    solve 10816

---
+++ 10816.cpp
@@ -0,0 +1,31 @@
+//
+// Created by apple on 2020-07-13.
+//
+// https://www.acmicpc.net/problem/10816
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    int *arr;
+
+    arr = (int *)malloc(sizeof(int) * 20000001);
+
+    for(int i = 0; i <= 20000000; i++)
+        arr[i] = 0;
+
+    for(cin >> n ; n > 0 ; n--) {
+        int in;
+        scanf("%d", &in);
+        arr[in + 10000000]++;
+    }
+
+    for(cin >> m; m > 0; m--) {
+        int in;
+        scanf("%d", &in);
+        printf("%d ", arr[in + 10000000]);
+    }
+
+    free(arr);
+}
