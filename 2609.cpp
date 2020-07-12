dff8e0e99de74070f3ae640d939f6b4a5e08f98e
Junhyeok99 <apple2870907@gmail.com>
2020-07-13 04:34:07 +0900

    solve 2609

---
+++ 2609.cpp
@@ -0,0 +1,21 @@
+//
+// Created by apple on 2020-07-13.
+//
+// https://www.acmicpc.net/problem/2609
+#include <iostream>
+
+using namespace std;
+
+int gcd(int i, int j) {
+    return j ? gcd(j, i % j) : i;
+}
+
+int main() {
+    int n, m;
+
+    cin >> n >> m;
+    int ans = gcd(n > m ? n : m, n > m ? m : n);
+
+    cout << ans << endl << n * m / ans;
+
+}
