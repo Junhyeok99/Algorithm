f0d0711f04e235cf00efd6dc590bc0dc63e8178d
Junhyeok99 <apple2870907@gmail.com>
2020-07-13 04:04:47 +0900

    solve 1259

---
+++ 1259.cpp
@@ -0,0 +1,38 @@
+//
+// Created by apple on 2020-07-13.
+//
+// https://www.acmicpc.net/problem/1259
+#include <iostream>
+
+using namespace std;
+
+bool isPal(int n) {
+    int arr[5];
+    int l = 0;
+
+    while(true) {
+        arr[l] = n % 10;
+        l++;
+        if(n / 10 == 0)
+            break;
+        n = n / 10;
+    }
+
+    for(int i = 0 ; i < l; i++) {
+        if(arr[i] != arr[l - 1 - i])
+            return false;
+    }
+
+    return true;
+}
+
+int main() {
+    while(true) {
+        int in;
+        scanf("%d", &in);
+        if(!in)
+            break;
+
+        printf("%s", isPal(in) ? "yes\n" : "no\n");
+    }
+}
