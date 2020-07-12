35139811cf10f4f48bcc872d8c4f5aa9563a0f8f
Junhyeok99 <apple2870907@gmail.com>
2020-07-13 05:09:49 +0900

    solve 10845

---
+++ 10845.cpp
@@ -0,0 +1,45 @@
+//
+// Created by apple on 2020-07-13.
+//
+// https://www.acmicpc.net/problem/10845
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int n;
+    queue<int> q;
+
+    for (cin >> n; n > 0; n--) {
+        string str;
+        cin >> str;
+
+        if(str == "push") {
+            int t;
+            scanf("%d", &t);
+            q.push(t);
+        } else if (str == "pop") {
+            if (q.empty())
+                printf("-1\n");
+            else {
+                printf("%d\n", q.front());
+                q.pop();
+            }
+        } else if (str == "size") {
+            printf("%lu\n", q.size());
+        } else if (str == "empty") {
+            printf("%d\n", q.empty() ? 1 : 0);
+        } else if (str == "front") {
+            if (q.empty())
+                printf("-1\n");
+            else
+                printf("%d\n", q.front());
+        } else if (str == "back") {
+            if (q.empty())
+                printf("-1\n");
+            else
+                printf("%d\n", q.back());
+        }
+    }
+}
