544a37712de88d64c5afcf5b5521365abe90c74c
Junhyeok99 <apple2870907@gmail.com>
2020-07-13 04:13:08 +0900

    solve 2164

---
+++ 2164.cpp
@@ -0,0 +1,26 @@
+//
+// Created by apple on 2020-07-13.
+//
+// https://www.acmicpc.net/problem/2164
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main() {
+    int n;
+    deque<int> d;
+
+    for(cin >> n; n > 0; n--)
+        d.push_front(n);
+
+    while(d.size() != 1) {
+        d.pop_front();
+        d.push_back(d.front());
+        d.pop_front();
+    }
+
+    cout << d.front();
+
+    return 0;
+}
