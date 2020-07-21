2a09251e928814668950ab8e5dd9a22d31b881c4
Junhyeok99 <apple2870907@gmail.com>
2020-07-22 01:02:43 +0900

    solve 1764

---
+++ 1764.cpp
@@ -0,0 +1,35 @@
+//
+// Created by apple on 2020-07-22.
+//
+// https://www.acmicpc.net/problem/1764
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    set<string> s;
+    set<string> ans;
+
+    for (cin >> n >> m; n > 0; n--) {
+        char name[21];
+        scanf("%s", name);
+        string str(name);
+
+        s.insert(str);
+    }
+
+    for (; m > 0; m--) {
+        char name[21];
+        scanf("%s", name);
+        string str(name);
+
+        if(s.find(str) != s.end())
+            ans.insert(str);
+    }
+
+    printf("%d\n", ans.size());
+    for(const auto& i : ans)
+        printf("%s\n", i.c_str());
+}
