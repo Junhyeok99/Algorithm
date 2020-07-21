52313cf7f8f0bbcc5517684805d61c2930473825
Junhyeok99 <apple2870907@gmail.com>
2020-07-21 19:36:31 +0900

    solve 1074

---
+++ 1074.cpp
@@ -0,0 +1,33 @@
+//
+// Created by apple on 2020-07-20.
+//
+// https://www.acmicpc.net/problem/1074
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int squ(int n, int r, int c) {
+    int add = 0;
+
+    if(n == 0)
+        return 0;
+
+    if (r >= pow(2, n - 1)) {
+        add += (int) pow(2, 2 * (n - 1)) * 2;
+        r -= pow(2, n - 1);
+    }
+    if (c >= pow(2, n - 1)) {
+        add += (int) pow(2, 2 * (n - 1));
+        c -= pow(2, n-1);
+    }
+
+    return add + squ(n-1, r, c);
+}
+
+int main() {
+    int n, r, c;
+    cin >> n >> r >> c;
+
+    cout << squ(n, r, c);
+}
