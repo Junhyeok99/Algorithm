f6fe6e2c7e3e03172487c60cb7c130188acfac64
Junhyeok99 <apple2870907@gmail.com>
2020-07-22 00:00:14 +0900

    solve 1620

---
+++ 1620.cpp
@@ -0,0 +1,37 @@
+//
+// Created by apple on 2020-07-21.
+//
+// https://www.acmicpc.net/problem/1620
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    map<string, int> s2i;
+    map<int, string> i2s;
+
+    cin >> n >> m;
+
+    for(int i = 1; i <= n; i++) {
+        char s[21];
+        scanf("%s", s);
+        string str(s);
+        s2i[str] = i;
+        i2s[i] = str;
+    }
+
+    for(;m > 0; m--) {
+        char s[21];
+        scanf("%s", s);
+        string str(s);
+
+        if('0' <= s[0] && '9' >= s[0])
+            printf("%s\n", i2s[stoi(str)].c_str());
+        else
+            printf("%d\n", s2i[str]);
+    }
+
+    return 0;
+}
