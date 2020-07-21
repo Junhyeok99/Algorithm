c4713eab4e94420ceb4d522c98dbfdf6b2ac7523
Junhyeok99 <apple2870907@gmail.com>
2020-07-22 02:52:17 +0900

    solve 1927

---
+++ 1927.cpp
@@ -0,0 +1,36 @@
+//
+// Created by apple on 2020-07-22.
+//
+// https://www.acmicpc.net/problem/1927
+#include <iostream>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main() {
+    int n;
+    set<int> s;
+    map<int, int> m;
+
+    for (cin >> n; n > 0; n--) {
+        int c;
+        scanf("%d", &c);
+
+        if (!c) {
+            if (s.empty())
+                printf("%d\n", 0);
+            else {
+                printf("%d\n", *s.begin());
+                m[*s.begin()]--;
+
+                if(m[*s.begin()] == 0) {
+                    s.erase(s.begin());
+                }
+            }
+        } else if (c > 0) {
+            m[c]++;
+            s.insert(c);
+        }
+    }
+}
