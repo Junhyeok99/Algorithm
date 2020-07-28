65772685f4c46d993ea9eb38df9de63513c55ecf
Junhyeok99 <apple2870907@gmail.com>
2020-07-28 11:21:49 +0900

    solve 11724

---
+++ 11399.cpp
@@ -0,0 +1,28 @@
+//
+// Created by apple on 2020-07-28.
+//
+// https://www.acmicpc.net/problem/11399
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    int sum = 0;
+    vector<int> v;
+
+    for (cin >> n; n > 0; n--) {
+        int t;
+        cin >> t;
+        v.push_back(t);
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i = v.size(); i > 0; i--)
+        sum += i * v[v.size() - i];
+
+    cout << sum;
+}
