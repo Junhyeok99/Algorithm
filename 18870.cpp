013bef319063f554125b23217b385742203902ea
Junhyeok99 <apple2870907@gmail.com>
2020-07-30 00:43:13 +0900

    solve 18870

---
+++ 18870.cpp
@@ -0,0 +1,34 @@
+//
+// Created by apple on 2020-07-30.
+//
+// https://www.acmicpc.net/problem/18870
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main() {
+    int n;
+    vector<int> v;
+    map<int, int> m;
+    set<int> s;
+
+    for(cin >> n; n > 0; n--) {
+        int t;
+        scanf("%d", &t);
+        v.push_back(t);
+        s.insert(t);
+    }
+
+    int c = 0;
+    for(auto i: s) {
+        m[i] = c++;
+    }
+
+    for(auto i: v)
+        printf("%d ", m[i]);
+
+}
